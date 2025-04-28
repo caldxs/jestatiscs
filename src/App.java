3b81f09155dcb23358a2aadce27d517943334f28
iara <iaraotako@gmail.com>
2025-04-28 11:57:53 -0300

    aula git

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
+import java.util.ArrayList;
+
+
+
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Alô, Git");
+        System.out.println(" \t\t JEstatistitics");
+        System.out.print("-- controle de dados Empresarial --");
+        System.out.print("2025");
+        //ArrayList lista = new ArrayList();
     }
 }
