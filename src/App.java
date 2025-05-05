a2b581bf731e1b3447638027d3ecafbfe1bbc6de
iara <iaraotako@gmail.com>
2025-05-05 11:58:54 -0300

    finalização aula3

--- src/App.java
+++ src/App.java
@@ -1,59 +1,62 @@
-
-import javax.swing.JOptionPane;
+import javax.swing.*;
 public class App {
-     public static void main(String[] args) throws Exception {
-        System.out.println("Aula- Switch e loops");
-        /*o switch é uma estrutura de seleção multipla 
-        um if mais organizado
-        Escolha
-          case
-        Para resolver listas ou constantes:
-          menu
-            opcao1
-            opcao2*/
-      /*   int op = Integer.parseInt(JOptionPane.showInputDialog("Digite a opção: \n 1- Estatística \n 2- Probabilidade \n 3- Modelos probabilistico \n 4-Encerrando sistema") );
-        if (op!= 4) {switch(op) {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Aula3- Switch e Loops");
+         String cont;
+         do{
+
+        int op = Integer.parseInt(JOptionPane.showInputDialog("Digite a opção: \n1-Estatística Descritiva \n2-Probabilidade \n3-Modelos probabilísticos \n 4-Sair"));
+        // !=  diferente
+            switch(op){
+                case 1:
+                JOptionPane.showMessageDialog(null,
+                 "Estatística Descritiva");break;
+                case 2:
+                JOptionPane.showMessageDialog(null,
+                 "Probabilidade");break;
+                case 3:
+                 JOptionPane.showMessageDialog(null,
+                  "Modelos Probabilisticos");break;
+
+                
+   
+                default:
+                JOptionPane.showMessageDialog(null,
+                "Opção Inválida!");break;
+  
+                
+             }
+            cont = JOptionPane.showInputDialog("Deseja Continuar? (S-Sim N-Não)");
+
+            JOptionPane.showMessageDialog(null, "Encerrando sistema...");
+          } while (cont.equalsIgnoreCase("S"));
+
+          JOptionPane.showMessageDialog(null, "Encerrando Sistema...");
+      
+    }
 
 
-          case 1:
-          JOptionPane.showInputDialog(null,"Estatística Desertativa");
-
-          break;
-
-          case 2:
-          JOptionPane.showInputDialog(null," Probabilidades");
-
-          break;
-
-          case 3:
-          JOptionPane.showInputDialog(null,"Estatistica descritiva");
-
-          break;
-
-          case 4: JOptionPane.showMessageDialog(null,"Encerrando sistema");
-
-
+       /* int contador = 1;
+        while(contador<1){
+           JOptionPane.showMessageDialog(null,"Viva o SENAI!!!");
+           contador = contador+1;          
         }
+        int i = 1;
+        while(i<=2){
+            System.out.println(i);
+            i++;
+        }
+        for(int cont=1;cont<=1;cont++){
+            System.out.println(cont);
+        }
+        String sair;
+        do{
+           //System.out.println("sair:s ou n");
+           sair = JOptionPane.showInputDialog("Digite s-sim e n-não");
+
+        }while(!sair.equalsIgnoreCase("sim"));
+        */
+
+      }
     
-    
-    }
-
-     /*int contador = 1;
-     while (contador<=5) {
-     JOptionPane.showMessageDialog(null,"Viva o senai");
-     break;
-     */
-
-     for(int cont=1; cont<=10;cont++){
-      System.out.println(cont);
-     }
-      String sair;
-      do{
-        sair = JOptionPane.showInputDialog("Digite s-sim e n-nao");
-       
-      }while (sair != "nao");
-    }
-    }
-
-
-
+  
