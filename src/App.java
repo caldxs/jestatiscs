21fdc5e27e11e9f4278379b37c7771523760277e
iara <iaraotako@gmail.com>
2025-05-12 08:24:58 -0300

    exercicios

--- src/App.java
+++ src/App.java
@@ -1,72 +1,34 @@
 import javax.swing.JOptionPane;
 
-
 public class App {
-    public static void main(String[] args) {
-       
-        String larguraStr = JOptionPane.showInputDialog("Quanto e a largura do seu jardim?");
-        String comprimentoStr = JOptionPane.showInputDialog("Qual o comprimento do seu jardim?");
+    
+    public static void main(String[] args) throws Exception {
+        
+        double areaJardim, comprimento, largura;
+        double somaArea = 0;
+        int areas=0;
+        int areasGrandes=0;
+        while (true) {
+            comprimento = Double.parseDouble(JOptionPane.showInputDialog(null,"Comprimento do Jardim: "));
+            largura = Double.parseDouble(JOptionPane.showInputDialog(null,"Largura do Jardim: "));
+            areas++;
+            
+            String[] tamanho = {"Pequeno","Grande"};
+            areaJardim = comprimento*largura;
 
-        double largura = Double.parseDouble(larguraStr.replace(",", "."));
-        double comprimento = Double.parseDouble(comprimentoStr.replace(",", "."));
-        double area = largura * comprimento;
-
-        String classificacao;
-        if (area >= 100) {
-            classificacao = "Grande";
-        } else {
-            classificacao = "Pequeno";
-        }
-
-        String resultado = String.format(
-            "A area do jardim e %.2f metros quadrados.\nO jardim e classificado como: %s.", 
-            area, classificacao
-        );
-
-        JOptionPane.showMessageDialog(null, resultado);
-
-        String codigoStr = JOptionPane.showInputDialog( "Informe o codigo do servico: \n 1 - Jardinagem, \n 2 -solo, \n 3 - Limpeza:"
-        );
-
-        int codigo = Integer.parseInt(codigoStr);
-        String descricao;
-
-        switch (codigo) {
-            case 1:
-                descricao = "Servico de Jardinagem: \n cuidado e manutencao de jardins.";
+            if (areaJardim < 100) {
+                JOptionPane.showMessageDialog(null, "Area do Jardim: "+areaJardim+"\nTamanho: "+tamanho[0]);
+            }else {
+                JOptionPane.showMessageDialog(null, "Area do Jardim: "+areaJardim+"\nTamanho: "+tamanho[1]);
+                areasGrandes++;
+            }
+            somaArea = somaArea + areaJardim;
+            int loop = JOptionPane.showConfirmDialog(null, "Deseja Adicionar mais um Terreno? ");
+            if (loop != JOptionPane.YES_OPTION) {     
                 break;
-            case 2:
-                descricao = "Servico de PreparaÁ„o do solo: \n  adubar e melhorar a qualidade do solo .";
-                break;
-            case 3:
-                descricao = "Servico de Limpeza: \n remover folhas secas, residuos, limpar caminhos e areas pavimentadas. .";
-                break;
-            default:
-                descricao = " Nenhum servico selecionado";
+            }
         }
-
-        JOptionPane.showMessageDialog(null, descricao);
-
-        String quantidadeStr = JOptionPane.showInputDialog(
-            "Informe quantos servico que esta querendo:"
-        );
-
-        int quantidade = Integer.parseInt(quantidadeStr);
-        double desconto;
-
-        if (quantidade >= 5) {
-            desconto = 10.0; // 
-        } else if (quantidade >= 3) {
-            desconto = 10.0; // 
-        } else {
-            desconto = 5.0; 
-        }
-
-        String mensagem = String.format(
-            "Voce solicitou %d servicos.\nDesconto valido de: %.0f%%.", 
-            quantidade, desconto
-        );
-
-        JOptionPane.showMessageDialog(null, mensagem);
+        double mediaArea = somaArea/areas;
+        JOptionPane.showMessageDialog(null, "A media das Areas:"+mediaArea+"\n¡reas Grandes: "+areasGrandes);
     }
 }
